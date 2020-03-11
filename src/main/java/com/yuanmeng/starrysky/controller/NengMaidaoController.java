3ae9351ab97b61139a1bd3a881b025005a4bac99
wangdongsheng <wangdongsheng@daixiaomi.com>
2020-03-11 18:59:29 +0800

    add login method

--- src/main/java/com/yuanmeng/starrysky/controller/NengMaidaoController.java
+++ src/main/java/com/yuanmeng/starrysky/controller/NengMaidaoController.java
@@ -1,15 +1,24 @@
-package com.yuanmeng.starrysky;
+package com.yuanmeng.starrysky.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping(path="/api")
+@RequestMapping(path="/api")
 public class NengMaidaoController {
 
     @RequestMapping(path="/check")
     public String check(){
         return "OK!";
+    }
+
+    @RequestMapping(path="/login")
+    public String login(@RequestBody JSONObject jsonParam){
+        System.out.println(jsonParam.toJSONString());
+        return "";
     }
 
 
