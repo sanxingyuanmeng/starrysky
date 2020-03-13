c7a885bbb60296a9a037ca7c4c16e9e5cb6df760
wangdongsheng <wangdongsheng@daixiaomi.com>
2020-03-13 17:19:43 +0800

    update login

--- src/main/java/com/yuanmeng/starrysky/common/WxUtils.java
+++ src/main/java/com/yuanmeng/starrysky/common/WxUtils.java
@@ -6,12 +6,13 @@ import com.alibaba.fastjson.TypeReference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
-import java.util.Base64;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import com.yuanmeng.starrysky.common.JsonUtils;
+import org.springframework.stereotype.Component;
 
+@Component
 public class WxUtils {
 
     //@Autowired
@@ -58,20 +59,16 @@ public class WxUtils {
 
     //获取openid/unionid/session_key字段
     public Map<String, String> getUserOpenId(){
-
-
-         /*
-        redis.set("user-redis-session:" + model.getOpenid(),
-                model.getSession_key(),
-                1000 * 60 * 30);
-        */
-
         String url = "https://api.weixin.qq.com/sns/jscode2session";
         Map<String, String> param = new HashMap<>();
         param.put("appid", this.appid);
         param.put("secret", this.secret_key);
         param.put("js_code", this.originalData.get("code"));
         param.put("grant_type", "authorization_code");
+
+        System.out.println("paraxxxx");
+        System.out.println(param);
+
         String wxResult = HttpClientUtil.doGet(url, param);
 
         JSONObject jsObject = JSONObject.parseObject(wxResult);
@@ -79,20 +76,26 @@ public class WxUtils {
         this.UserIdInfo =JSONObject.parseObject(jsObject.toJSONString(),
                 new TypeReference<Map<String, String>>(){});
 
+        System.out.println(this.UserIdInfo);
         return this.UserIdInfo;
     }
 
 
     //信息解密
-    public String decrypt(String session_key, String encrypted_data, String iv){
-        final Base64.Decoder decoder = Base64.getDecoder();
-        String dec_session_key = new String(decoder.decode(session_key));
-        String dec_iv = new String(decoder.decode(iv));
-        String dec_encrypted_data = new String(decoder.decode(encrypted_data));
-        String wx_json = AES.wxDecrypt(dec_encrypted_data, dec_session_key, dec_iv);
-        return wx_json;
+    public void decrypt(String session_key, String encrypted_data, String iv){
+        System.out.println("session key   " + session_key);
+        System.out.println("encrypted_data   " + encrypted_data);
+        System.out.println("iv   " + iv);
 
-
+        String wx_json = AES.wxDecrypt(encrypted_data, session_key, iv);
+        this.UserData = JSONObject.parseObject(wx_json,
+                new TypeReference<Map<String, String>>(){});
     }
 
+    //封装getUserOpenId和decrypt，用户在登录的时候直接使用
+    public void decryptUserInfo(){
+        this.getUserOpenId();
+        this.decrypt(this.UserIdInfo.get("session_key"), this.originalData.get("encryptedData"), this.originalData.get("iv"));
+
+    }
 }
---
+++ src/main/java/com/yuanmeng/starrysky/controller/NengMaidaoController.java
@@ -0,0 +1,57 @@
+package com.yuanmeng.starrysky.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.yuanmeng.starrysky.common.RedisOperator;
+import com.yuanmeng.starrysky.common.WxUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+
+@RequestMapping(path="/api")
+public class NengMaidaoController {
+
+    @Autowired
+    private WxUtils wx;
+
+    @Autowired
+    private RedisOperator redis;
+
+    @RequestMapping(path="/check")
+    public String check(){
+        return "OK!";
+    }
+
+    @RequestMapping(path="/login")
+    public String login(@RequestBody JSONObject jsonParam){
+        System.out.println(jsonParam.toJSONString());
+        wx.setOriginalData(jsonParam);
+
+        //与微信服务端交互并解密用户信息
+        wx.decryptUserInfo();
+
+        Map<String, String>User = wx.getUserData();
+        for(String key:User.keySet()){
+            System.out.println(key + User.get(key));
+        }
+        return "";
+    }
+
+    /*
+    @RequestMapping(path = "/checkredis")
+    public String checkredis(){
+        redis.set("1234", "4321", 1000*30);
+        String res = redis.get("1234");
+        return res;
+    }
+
+     */
+
+
+}
