d4b2cb1aa130b712aa8b1d5263504188ce46ecc8
wangdongsheng <wangdongsheng@daixiaomi.com>
2020-03-15 14:02:36 +0800

    add reponse util

--- src/main/java/com/yuanmeng/starrysky/controller/UserController.java
+++ src/main/java/com/yuanmeng/starrysky/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.yuanmeng.starrysky.controller;
 
 
+import com.alibaba.fastjson.JSONObject;
+import com.yuanmeng.starrysky.common.RedisOperator;
+import com.yuanmeng.starrysky.common.WxUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * <p>
@@ -16,5 +23,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private WxUtils wx;
+
+    @Autowired
+    private RedisOperator redis;
+
+    @RequestMapping(path="/check")
+    public String check(){
+        return "OK!";
+    }
+
+    @RequestMapping(path="/login")
+    public String login(@RequestBody JSONObject jsonParam){
+        System.out.println(jsonParam.toJSONString());
+        wx.setOriginalData(jsonParam);
+
+        //与微信服务端交互并解密用户信息
+        wx.decryptUserInfo();
+
+        Map<String, String> User = wx.getUserData();
+        for(String key:User.keySet()){
+            System.out.println(key + User.get(key));
+        }
+        return "";
+    }
+
+    /*
+    @RequestMapping(path = "/checkredis")
+    public String checkredis(){
+        redis.set("1234", "4321", 1000*30);
+        String res = redis.get("1234");
+        return res;
+    }
+
+     */
 
 }
