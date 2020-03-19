113d074d7b60f003bfcf1b152987bb406fec2084
zhangshaohua <zhangshaohua@daixiaomi.com>
2020-03-19 23:41:35 +0800

    add shiro config

---
+++ src/main/java/com/yuanmeng/starrysky/config/CustomRealm.java
@@ -0,0 +1,98 @@
+package com.yuanmeng.starrysky.config;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CustomRealm extends AuthorizingRealm {
+
+    //@Autowired
+    //private LoginService loginService;
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        //获取登录用户名
+        /*
+        String name = (String) principalCollection.getPrimaryPrincipal();
+        //根据用户名去数据库查询用户信息
+        User user = loginService.getUserByName(name);
+        //添加角色和权限
+        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+        for (Role role : user.getRoles()) {
+            //添加角色
+            simpleAuthorizationInfo.addRole(role.getRoleName());
+            //添加权限
+            for (Permissions permissions : role.getPermissions()) {
+                simpleAuthorizationInfo.addStringPermission(permissions.getPermissionsName());
+            }
+        }
+        return simpleAuthorizationInfo;
+
+         */
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        //加这一步的目的是在Post请求的时候会先进认证，然后在到请求
+        if (authenticationToken.getPrincipal() == null) {
+            return null;
+        }
+        //获取用户信息
+        String name = authenticationToken.getPrincipal().toString();
+        //User user = loginService.getUserByName(name);
+        if (name == null) {
+            //这里返回后会报出对应异常
+            return null;
+        } else {
+            //这里验证authenticationToken和simpleAuthenticationInfo的信息
+            //SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(name, user.getPassword().toString(), getName());
+            //return simpleAuthenticationInfo;
+            return null;
+        }
+    }
+}
+
+/**
+
+public class CustomRealm extends AuthorizingRealm {
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        String username = (String) SecurityUtils.getSubject().getPrincipal();
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+        Set<String> stringSet = new HashSet<>();
+        stringSet.add("user:show");
+        stringSet.add("user:admin");
+        info.setStringPermissions(stringSet);
+        return info;
+    }
+
+    /**
+     * 这里可以注入userService,为了方便演示，我就写死了帐号了密码
+     * private UserService userService;
+     * <p>
+     * 获取即将需要认证的信息
+     *
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        System.out.println("-------身份认证方法--------");
+        String userName = (String) authenticationToken.getPrincipal();
+        String userPwd = new String((char[]) authenticationToken.getCredentials());
+        //根据用户名从数据库获取密码
+        String password = "123";
+        if (userName == null) {
+            throw new AccountException("用户名不正确");
+        } else if (!userPwd.equals(password )) {
+            throw new AccountException("密码不正确");
+        }
+        return new SimpleAuthenticationInfo(userName, password,getName());
+    }
+}
+**/
